3c0cf1eb7b0d2758bd915383d290c0783cb0077c
Stephanie Neumann <ahoi@stephanieneumann.com>
2019-10-15 21:38:10 +0200

    small update disciplines

--- js/settings.js
+++ js/settings.js
@@ -1,10 +1,10 @@
 // TODO: REDUCE NUMBER OF CATEGORIES - We only have 7 different symbols
 let symbolTypes = [
   {"discipline": 1,"name": "painting / drawing / collage","symbol": 'symbolCircle'},
-  {"discipline": 2,"name": "installation / performance / generative art","symbol": 'symbolCross'},
+  {"discipline": 2,"name": "installation / performance ","symbol": 'symbolCross'},
   {"discipline": 3,"name": "music / sound","symbol": 'symbolDiamond'},
   {"discipline": 4,"name": "photography / film","symbol": 'symbolSquare'},
   {"discipline": 5,"name": "conceptual art / mixed media","symbol": 'symbolStar'},
-  {"discipline": 6,"name": "sculpture / sculptural painting","symbol": 'symbolTriangle'},
+  {"discipline": 6,"name": "sculpture","symbol": 'symbolTriangle'},
   {"discipline": 7,"name": "writing","symbol": 'symbolWye'}
 ]
