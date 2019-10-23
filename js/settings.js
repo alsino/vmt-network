ca5a7fb688ccfe6b1f3e82cb8aa24a240a2a135a
alsino <iamahorse@gmail.com>
2019-10-23 17:03:51 +0200

    Linjk types added to dictionary

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,4 @@
-// TODO: REDUCE NUMBER OF CATEGORIES - We only have 7 different symbols
-let symbolTypes = [
+const symbolTypes = [
   {"discipline": 1,"name": "Painting / Drawing / Collage","symbol": 'symbolCircle', "selected": false},
   {"discipline": 2,"name": "Installation / Performance / Experience","symbol": 'symbolCross', "selected": false},
   {"discipline": 3,"name": "Music / Sound", "symbol": 'symbolDiamond', "selected": false},
@@ -7,4 +6,10 @@ let symbolTypes = [
   {"discipline": 5,"name": "Conceptual art / Mixed media","symbol": 'symbolStar', "selected": false},
   {"discipline": 6,"name": "Sculpture","symbol": 'symbolTriangle', "selected": false},
   {"discipline": 7,"name": "Writing","symbol": 'symbolWye', "selected": false}
+]
+
+const linkTypes = [
+  {"type": "Exhibited together / on stage together", "value": 10, "color": "rgba(0, 5, 255, 0.3)"};
+  {"type": "Together in a residency / program", "value": 15, "color": "rgba(0,255,0,0.3)"};
+  {"type": "Collaborated on a project / art piece ", "value": 25, "color": "rgba(255,0,0,0.3)"};
 ]
