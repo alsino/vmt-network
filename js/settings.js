58eaef6a3844f2e679f648d2ebeaf6a480d2137f
Stephanie Neumann <ahoi@stephanieneumann.com>
2019-10-25 12:11:09 +0200

    update disciplines

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,6 @@
 const symbolTypes = [
   {"discipline": 1,"name": "Painting / Drawing / Collage","symbol": 'symbolCircle', "selected": false},
-  {"discipline": 2,"name": "Installation / Performance / Experience","symbol": 'symbolCross', "selected": false},
+  {"discipline": 2,"name": "Installation / Performance ","symbol": 'symbolCross', "selected": false},
   {"discipline": 3,"name": "Music / Sound", "symbol": 'symbolDiamond', "selected": false},
   {"discipline": 4,"name": "Photography / Film","symbol": 'symbolSquare', "selected": false},
   {"discipline": 5,"name": "Conceptual art / Mixed media","symbol": 'symbolStar', "selected": false},
